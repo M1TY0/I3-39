1b728980038a9cfea3912d21d85edcd0fdb0d9ad
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2022-12-13 14:53:25 +0200

    ready

---
+++ I3-39/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+if (n == 60)
+{
+    Console.WriteLine("триъгълник");
+}
+else if (n == 90)
+{
+    Console.WriteLine("триъгълник");
+}
+else if (n == 108)
+{
+    Console.WriteLine("триъгълник");
+}
+else if (n == 120)
+{
+    Console.WriteLine("триъгълник");
+}
